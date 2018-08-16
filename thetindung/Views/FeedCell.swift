6bb5e9c2ef719873d5c88cd599d092a434ae8855
Tiny World <tinyworld.app@gmail.com>
2018-08-17 01:02:42 +0700

    add custom control refresh

--- thetindung/Views/FeedCell.swift
+++ thetindung/Views/FeedCell.swift
@@ -19,14 +19,19 @@ class FeedCell: BaseCell, UICollectionViewDataSource, UICollectionViewDelegate, 
         return cv
     }()
     
+    var footerView:RefreshControlView?
+    
     var videos: [Video]?
     var creditCards: [CreditCard]?
+    var isLoading:Bool = false
     
     let cellId = "cellId"
+    let footerViewReuseIdentifier = "RefreshFooterView"
+    
+    private let refreshControl = UIRefreshControl()
     
     func fetchVideos() {
         ApiService.sharedInstance.fetchVideos { (videos: [Video]) in
-
             self.videos = videos
             self.collectionView.reloadData()
 
@@ -35,27 +40,31 @@ class FeedCell: BaseCell, UICollectionViewDataSource, UICollectionViewDelegate, 
     
     func fetchCreditCards() {
         FirestoreSerivce.sharedInstance.fetchCards { (creditCards: [CreditCard]) in
-            self.creditCards  = creditCards
+            self.creditCards = creditCards
             self.collectionView.reloadData()
             
         }
+    }
+    
+    @objc private func refreshWeatherData(_ sender: Any) {
+        // Fetch Weather Data
+        fetchCreditCards()
     }
 
     override func setupViews() {
         super.setupViews()
         
+        refreshControl.addTarget(self, action: #selector(refreshWeatherData(_:)), for: .valueChanged)
         //fetchVideos()
-        
         fetchCreditCards()
         backgroundColor = .brown
-        
+        collectionView.refreshControl = refreshControl
         addSubview(collectionView)
         addConstraintsWithFormat("H:|[v0]|", views: collectionView)
         addConstraintsWithFormat("V:|[v0]|", views: collectionView)
-        
         //collectionView.register(VideoCell.self, forCellWithReuseIdentifier: cellId)
-        
         collectionView.register(CreditCardCell.self, forCellWithReuseIdentifier: cellId)
+        collectionView.register(UINib(nibName: "RefreshControlView", bundle: nil), forSupplementaryViewOfKind: UICollectionElementKindSectionFooter, withReuseIdentifier: footerViewReuseIdentifier)
     }
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
@@ -67,12 +76,6 @@ class FeedCell: BaseCell, UICollectionViewDataSource, UICollectionViewDelegate, 
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-//        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cellId", for: indexPath) as! VideoCell
-//
-//        cell.video = videos?[indexPath.item]
-//
-//        return cell
-        
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cellId", for: indexPath) as! CreditCardCell
         
         cell.creditCard = creditCards?[indexPath.item]
@@ -81,35 +84,106 @@ class FeedCell: BaseCell, UICollectionViewDataSource, UICollectionViewDelegate, 
     }
     
     
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+//    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+//        let height = (frame.width - 16 - 16) * 9 / 16
+//        return CGSize(width: frame.width, height: height + 16 + 88)
+//    }
+    
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        let height = (frame.width - 16 - 16) * 9 / 16
-        return CGSize(width: frame.width, height: height + 16 + 88)
+        return CGSize(width: frame.width, height: frame.height/2)
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
         return 0
     }
-
+    
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
+        return CGSize.zero
+    }
+    
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         //let videoLauncher = VideoLauncher()
         //videoLauncher.showVideoPlayer()
     }
     
-    func scrollViewDidScroll(_ scrollView: UIScrollView) {
-        // calculates where the user is in the y-axis
-        let offsetY = scrollView.contentOffset.y
-        let contentHeight = scrollView.contentSize.height
-        
-        if offsetY > contentHeight - scrollView.frame.size.height {
-            // call your API for more data
-           updateNextSet()
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForFooterInSection section: Int) -> CGSize {
+        if self.isLoading {
+            return CGSize.zero
+        }
+        return CGSize(width: collectionView.bounds.size.width, height: 55)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+        if kind == UICollectionElementKindSectionFooter {
+            let aFooterView = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: footerViewReuseIdentifier, for: indexPath) as! RefreshControlView
+            self.footerView = aFooterView
+            self.footerView?.backgroundColor = UIColor.clear
+            return aFooterView
+        } else {
+            let headerView = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: footerViewReuseIdentifier, for: indexPath)
+            return headerView
         }
     }
     
-    func updateNextSet(){
-        print("On Completetion")
-        //requests another set of data (20 more items) from the server.
+    func collectionView(_ collectionView: UICollectionView, willDisplaySupplementaryView view: UICollectionReusableView, forElementKind elementKind: String, at indexPath: IndexPath) {
+        if elementKind == UICollectionElementKindSectionFooter {
+            self.footerView?.prepareInitialAnimation()
+        }
     }
+    
+    func collectionView(_ collectionView: UICollectionView, didEndDisplayingSupplementaryView view: UICollectionReusableView, forElementOfKind elementKind: String, at indexPath: IndexPath) {
+        if elementKind == UICollectionElementKindSectionFooter {
+            self.footerView?.stopAnimate()
+        }
+    }
+    
+    //compute the scroll value and play witht the threshold to get desired effect
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        let threshold   = 100.0 ;
+        let contentOffset = scrollView.contentOffset.y;
+        let contentHeight = scrollView.contentSize.height;
+        let diffHeight = contentHeight - contentOffset;
+        let frameHeight = scrollView.bounds.size.height;
+        var triggerThreshold  = Float((diffHeight - frameHeight))/Float(threshold);
+        triggerThreshold   =  min(triggerThreshold, 0.0)
+        let pullRatio  = min(fabs(triggerThreshold),1.0);
+        self.footerView?.setTransform(inTransform: CGAffineTransform.identity, scaleFactor: CGFloat(pullRatio))
+        if pullRatio >= 1 {
+            self.footerView?.animateFinal()
+        }
+        print("pullRation:\(pullRatio)")
+    }
+    
+    //compute the offset and call the load method
+    func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
+        let contentOffset = scrollView.contentOffset.y;
+        let contentHeight = scrollView.contentSize.height;
+        let diffHeight = contentHeight - contentOffset;
+        let frameHeight = scrollView.bounds.size.height;
+        let pullHeight  = fabs(diffHeight - frameHeight);
+        print("pullHeight:\(pullHeight)");
+        if pullHeight == 0.0
+        {
+            if (self.footerView?.isAnimatingFinal)! {
+                print("load more trigger")
+                self.isLoading = true
+                self.footerView?.startAnimate()
+                FirestoreSerivce.sharedInstance.fetchCards { (creditCards: [CreditCard]) in
+                    self.creditCards?.append(contentsOf: creditCards)
+                    self.collectionView.reloadData()
+                    self.isLoading = false
+                }
+            }
+        }
+    }
+
+
+    
 
 
 }
